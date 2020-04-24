df243f9bd9d0c0184c65d8747ec76254ee79dcb4
김혜린 <@DESKTOP-SKFN5AL>
2020-04-24 11:13:28 +0900

    세번쨰 커밋

--- gitTest/src/git/Git.java
+++ gitTest/src/git/Git.java
@@ -4,5 +4,7 @@ public class Git {
 
 	public static void main(String[]args) {
 		System.out.println("hello");
-	}
-}
+		System.out.println("World");
+		System.out.println("thanks");
+		// 잘올라갔겠지
+	}}
